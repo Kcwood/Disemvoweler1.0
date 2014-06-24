08bd7a04bb6a6b9004bfa9b1988aedd93bbc1c10
Kyle Wood <kyle.c.wood88@gmail.com>
2014-06-24 14:57:28 -0600

    Finished Disemvoweler
    Moved print lines outside of for loop

--- Program.cs
+++ Program.cs
@@ -11,15 +11,17 @@ namespace Disemvoweler
         static void Main(string[] args)
         {
             //This program is going to be awesome
-            
+            Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!");
             Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?");
             Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
-            Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!");
+            
             Console.ReadKey();
         }
         //creating the function for the Disemvoweler
         static void Disemvoweler(string input)
         {
+            //setting our input to lower case letters
+            input = input.ToLower();
             //declaring funtion variables
             int i = 0;
             string consonantResult = "";
@@ -28,7 +30,7 @@ namespace Disemvoweler
             //and take out all the vowels
             for (i = 0; i < input.Length; i++)
             {
-            //setting our index
+                //setting our index
                 char letter = input[i];
                 //pulling out the consonants and 
                 //printing them to the console in order
@@ -41,14 +43,15 @@ namespace Disemvoweler
                 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
                 {
                     vowelResult += letter;
-                }
+                }  
+            }
             //writing to the console to show our
             //our original input and then the 
             //input disemvoweled
-                Console.WriteLine("Original: " + input);
-                Console.WriteLine("Disemvoweled: " + consonantResult);
-                Console.WriteLine("Vowels: " + vowelResult);
-            }
+            Console.WriteLine("Original: " + input); 
+            Console.WriteLine("Disemvoweled: " + consonantResult);
+            Console.WriteLine("Vowels: " + vowelResult);
+            Console.WriteLine();
         }
     }
 }
